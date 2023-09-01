a45c3870b5d5069d17e79796df4c9e8e5e362c05
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-09-01 19:11:56 +0200

    nav bar done

--- app.js
+++ app.js
@@ -2,9 +2,25 @@
 
 const navMenu = document.querySelector('.nav__bar');
 const mobileNav = document.querySelector('.mobile__navbar');
+const mobileNavLinks = document.querySelectorAll('.mobile-nav__link')
+
+console.log(mobileNavLinks);
+
 
 
 navMenu.addEventListener('click', () => {
   mobileNav.classList.toggle('hidden')
 
+});
+
+mobileNavLinks.forEach(link => {
+
+  link.addEventListener('click', () => {
+    let isNavOpen = link.classList.contains('hidden');
+
+    if (isNavOpen) {
+      mobileNav.classList.remove('hidden')
+    }
+
+  })
 })
