683334f10518468552a0c329559646222b30c21e
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-09-02 18:15:11 +0200

    some changes

--- src/app.js
+++ src/app.js
@@ -5,11 +5,19 @@ const form = document.querySelector('form');
 const input = document.querySelector('.links');
 const submitBtn = document.querySelector('.links__btn');
 const shortenedLink = document.querySelector('.shorten__link');
+const warning = document.querySelector('.warn');
 
 // adding an Event Handler to the Form
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const longUrl = input.value;
+  let longUrl = '';
+
+  if (input.value === '') {
+    warning.classList.remove('hidden');
+    input.classList.add('warning');
+  } else {
+    longUrl = input.value;
+  }
 
   shortenUrlAPI(longUrl);
 });
@@ -21,13 +29,25 @@ async function shortenUrlAPI(longUrl) {
 
     const data = await response.json();
 
+    //appending data result on the HTML
     shortenedLink.innerHTML = `
     <h4>${input.value}</h4>
     <p>${data.result.short_link}</p>
     <button class="copy btn">Copy</button>
     `;
 
+    //creating copy functionality
+    const copyBtn = document.querySelector('.copy');
+    copyBtn.addEventListener('click', () => {
+      navigator.clipboard.writeText(copyBtn.previousElementSibling.textContent);
+      copyBtn.textContent = 'Copied!';
+      copyBtn.style.cssText += 'color:#fff;background-color:#3b3054';
+      input.value = '';
+    });
+
+
   } catch (error) {
+    console.log(error);
 
   }
 }
